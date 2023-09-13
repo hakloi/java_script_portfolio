0b9cd6ef8c0897c56094242f92d6c149f3a69035
Violetta <murasakitta@gmail.com>
2023-09-13 08:15:36 +0300

    canvas js Mondrian FINISH

--- Mondrian/sketch.js
+++ Mondrian/sketch.js
@@ -2,6 +2,7 @@ function setup()
 {
 	//create a large square canvas
 	createCanvas(800, 800);
+    background(255,255,100);
 }
 
 function draw()
@@ -14,6 +15,29 @@ function draw()
 	strokeWeight(12);
 
 	//draw the red rectangle 
-	rect(100, 50, 600, 600);
-
+	rect(210, 0, 600, 600);
+    
+    fill(255,250,250);
+    rect(0,0,200,250);
+    
+    fill(0,0,0);
+    rect(0,250,200,40);
+    
+    fill(255,250,250);
+    rect(0,300,200,300);
+    
+    fill(0,0,0);
+    rect(0,600,800,20);
+    
+    fill(65,105,225);
+    rect(0,620,200,200);
+    
+    fill(255,250,250);
+    rect(210,620,490,500);
+    
+    fill(250,255,255);
+    rect(720,620,100,100);
+    
+    fill(0,0,0)
+    rect(700,630,20,200);
 }
