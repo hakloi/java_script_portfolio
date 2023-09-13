9a46aa2cc99ec4ad2378095b325a54615a7cf8c2
Violetta <murasakitta@gmail.com>
2023-09-13 07:43:56 +0300

    canvas js robot FINISH

--- hack_it_robot_1/sketch.js
+++ hack_it_robot_1/sketch.js
@@ -8,61 +8,137 @@ function setup()
 function draw()
 {
 	strokeWeight(6);
-    
-//    //body
-//    fill(240,128,128);
-//    rect(150,250,120,100,0); // x, y, a, b, округление 
-//    // hand (back)
-//    fill(138,43,226);
-//    rect(130,160,30,60,0);
-//    // bottom
-//    fill(220,20,60);
-//    rect(140,170,100,90,0);
-//    // head and neck
-//    fill(255,228,225);
-//    rect(130,70,70,70,0);
-//    fill(255,228,225);
-//    rect(150,140,30,30,0);
-//    //hand (front)
-//    fill(153,50,204);
-//    rect(160,160,30,60,0);
-    
-    ellipse(175, 200, 80,80)
-    point(325, 200)
-    
 
-//	//robots antenna
-//	fill(250, 250, 0);
-//	ellipse(250, 70, 60, 60);
-//
-//	fill(200, 0, 200);
-//	rect(210, 80, 80, 30);
-//
-//	//robots eyes
-//	fill(255);
-//	ellipse(175, 200, 80, 80);
-//	point(175, 200);
-//	ellipse(325, 200, 80, 80);
-//	point(325, 200);
-//
-//
-//	//robots nose
-//	fill(255, 0, 0);
-//	triangle(250, 220, 200, 300, 300, 300);
-//
-//	//robots ears
-//	rect(80, 180, 30, 100);
-//	rect(390, 180, 30, 100);
-//
-//	//robots mouth
-//	noFill();
-//	beginShape();
-//	vertex(175, 340);
-//	vertex(200, 370);
-//	vertex(225, 340);
-//	vertex(250, 370);
-//	vertex(275, 340);
-//	vertex(300, 370);
-//	vertex(325, 340);
-//	endShape();
+    // robo body (cloth,neck)
+    fill(195, 247, 136);
+    ellipse(415, 520, 445, 345);
+    fill(255, 260, 115);
+    beginShape();
+    vertex(435,320);
+    vertex(460,450);
+    vertex(510,470);
+    vertex(530,330);
+    endShape();
+    
+    
+    //robots ears
+    fill(200, 0, 200);
+	rect(240, 150, 30, 100);
+
+    
+    // face line
+    fill(255, 260, 115);
+    beginShape();
+    vertex(250,0);
+    vertex(260,200);
+    vertex(300,300);
+    vertex(330,350);
+    vertex(400,380);
+    vertex(450,380); //прямая подбородок
+    vertex(520,350);
+    vertex(530,0);
+    endShape();
+    
+    // robo-hair
+    fill(115, 139, 255);
+    beginShape();
+    vertex(270,230);
+    vertex(250,300);
+    vertex(200,290);
+    vertex(260,350);
+    vertex(300,370);
+    vertex(370,370);
+    
+    endShape();
+    
+    fill(115, 139, 255);
+	triangle(470, 130, 500, 0, 380, 0);
+    
+    fill(115, 139, 255);
+	triangle(250, 90, 290, 0, 250, 0); // x1, y1, x2, y2, x3, y3
+    fill(115, 139, 255);
+	triangle(270, 140, 290, 0, 350, 0);
+    
+    fill(115, 139, 255);
+	triangle(250, 200, 200, 0, 250, 0);
+    
+	//robots antenna
+	fill(195, 247, 136);
+	ellipse(150, 100, 60, 60);
+
+	fill(200, 0, 200);
+	beginShape();
+    vertex(150,70);
+    vertex(180,0);
+    vertex(190,0);
+    vertex(160,70);
+    endShape();
+
+	//robots eyes
+	ellipse(325, 150, 75, 5);
+    fill(0,0,0);
+    beginShape();
+    vertex(370,150);
+    vertex(360,170);
+    endShape();
+    
+    fill(0,0,0);
+    beginShape();
+    vertex(278,145);
+    vertex(290,150);
+    endShape();
+    
+    noFill();
+    beginShape();
+    vertex(440,110);
+    vertex(470,100);
+    vertex(500,110);
+    endShape();
+    
+    noFill();
+    beginShape();
+    vertex(450,170);
+    vertex(480,180);
+    vertex(500,170);
+    endShape();
+    
+    fill(0, 255, 255);
+    ellipse(475, 140, 45, 40);
+    point(475,140);
+
+	//robots nose
+	fill(255, 192, 255);
+	beginShape();
+    vertex(400,200);
+    vertex(390,230);
+    vertex(400,240);
+    endShape();
+    
+    noFill();
+	beginShape();
+	vertex(360, 140);
+	vertex(340, 120);
+	endShape();
+
+	//robots mouth
+    fill(255, 192, 255);
+    ellipse(410, 300, 45, 15);
+    
+    // eyebrows
+    fill(115, 139, 255);
+    beginShape();
+    vertex(280,100);
+    vertex(360,80);
+    vertex(360,90);
+    vertex(350,95);
+    endShape();
+    
+    fill(115, 139, 255);
+    beginShape();
+    vertex(500,100);
+    vertex(430,80);
+    vertex(430,90);
+    vertex(440,95);
+    endShape();
+    
 }
