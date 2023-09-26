fb03a75edb584e3540e1fe4c5c8799f551586313
Violetta <murasakitta@gmail.com>
2023-09-27 02:39:10 +0300

    js abstract art

--- Hw2/Kandinsky/sketch.js
+++ Hw2/Kandinsky/sketch.js
@@ -1,17 +1,203 @@
 function setup()
 {
-	createCanvas(500,500);
-    background(100, 100, 250);
-    fill(250, 100, 100);
-    square(20, 20, 60);
-    erase();
-    circle(25, 30, 30);
-    noErase();
-    describe('A purple canvas with a pink square in the middle. A circle is erased from the top-left, leaving a white hole.');
+	createCanvas(550, 650);
+	background(221, 194, 175); //45, 26, 59
+
 }
 
 function draw()
 {
-	//do your drawing here
+    noStroke()
+    fill(104, 222, 206);
+    rect(0,70,70,70);
+    rect(0,190, 70,70);
+    rect(0,210,70 ,70);
+    
+    rect(20,30,10,10);
+    rect(20,50, 10,10);
+    rect(20,70,10 ,10);
+    
+    rect(50,30,10,10);
+    rect(50,50, 10,10);
+    rect(50,70,10 ,10);
+    
+    rect(20,70,10,10);
+    rect(20,70, 10,10);
+    
+    rect(40,40,10,10);
+    rect(40,60, 10,10);
+    
+    fill(53, 161, 53);
+    beginShape();
+    vertex(145,0);
+    vertex(150,0);
+    vertex(0,650);
+    vertex(0,500);
+    vertex(0,200);
+    endShape();
+    
+    fill(219, 61, 148);
+    rect(400,100,100,170);
+    
+    fill(240, 225, 113);
+    rect(130,40,130,190);
+    rect(390,300,130,290)
+    
+    
+    fill(103, 198, 207);
+    ellipse(500,370,160,160);
+    
+    fill(100, 48, 191);
+    beginShape();
+    vertex(490,200);
+    vertex(310,70);
+    vertex(320,450);
+    endShape();
+    
+    fill(117, 95, 80);
+	beginShape();
+    vertex(120,170);
+    vertex(200,450);
+    vertex(450,350);
+    vertex(440, 200);
+    endShape();
 	
+    
+    fill(0,0,0);
+    beginShape();
+    vertex(20,520);
+    vertex(35,530);
+    vertex(520,170);
+    vertex(540, 175);
+    endShape();
+    
+    ellipse(400,210,90,90);
+    
+    
+    beginShape();
+    vertex(220,230);
+    vertex(210,270);
+    vertex(190,250);
+    vertex(210,190);
+    vertex(300,320);
+    vertex(300,360);
+    vertex(400,380);
+    vertex(500,490);
+    endShape();
+    
+    fill(350,350,350);
+    ellipse(400,210,70,70);
+
+    beginShape();
+    vertex(390,200);
+    vertex(310,170);
+    vertex(320,150);
+    endShape();
+    
+    rect(410,230,50,50);
+    
+    fill(0,0,0);
+    rect(410,230,10,10);
+    rect(410,250, 10,10);
+    rect(410,270,10 ,10);
+    
+    rect(430,230,10,10);
+    rect(430,250, 10,10);
+    rect(430,270,10 ,10);
+    
+    rect(450,230,10,10);
+    rect(450,250, 10,10);
+    rect(450,270,10 ,10);
+    
+    rect(420,240,10,10);
+    rect(420,260, 10,10);
+    
+    rect(440,240,10,10);
+    rect(440,260, 10,10);
+    
+    fill(219, 61, 148);
+    triangle(400,200,200,10,400,30);
+    
+    fill(104, 222, 206);
+    ellipse(170,460,250,250);
+    ellipse(500,210,90,100);
+    
+    fill(155, 137, 201);
+    beginShape();
+    vertex(100,400);
+    vertex(190,40);
+    vertex(120,50);
+    endShape();
+    
+    fill(91, 191, 227);
+    beginShape();
+    vertex(490,400);
+    vertex(400,490);
+    vertex(420,510);
+    endShape();
+    
+    fill(0,0,0);
+    ellipse(170,250,90,70);
+    fill(201, 73, 171);
+    ellipse(170,240,70,40)
+    
+    fill(227, 179, 91);
+    beginShape();
+    vertex(200,400);
+    vertex(250,500);
+    vertex(450,500);
+    endShape();
+    
+    fill(201, 70, 160);
+    rect(330,400,70,30);
+    fill(201, 73, 230);
+    rect(320,430,80,40);
+    
+    fill(138, 98, 90);
+    rect(250,500,120,130);
+    
+    fill(200,30,330);
+    beginShape();
+    vertex(0,400);
+    vertex(80,400);
+    vertex(120,500);
+    vertex(250,550);
+    vertex(300,600);
+    vertex(500,650);
+    vertex(0,650);
+    endShape();
+    
+    fill(134,8,94);
+    rect(180,40,120,120)
+    
+    rect(160,270,50,50);
+    
+    fill(0,0,0);
+    rect(160,270,10,10);
+    rect(160,290, 10,10);
+    rect(160,310,10 ,10);
+    
+    rect(180,270,10,10);
+    rect(180,290, 10,10);
+    rect(180,310,10 ,10);
+    
+    rect(200,270,10,10);
+    rect(200,290, 10,10);
+    rect(200,310,10 ,10);
+    
+    fill(54,67,130);
+    rect(220,240,10,10);
+    rect(220,260, 10,10);
+    
+    rect(240,240,10,10);
+    rect(240,260, 10,10);
+    
+    rect(80,230,30,130);
+    rect(460,230,30,130);
+    rect(460,230,30,130);
+    
+    fill(345,34,76);
+    triangle(340,57,12,345,67);
+    
+    
 }
